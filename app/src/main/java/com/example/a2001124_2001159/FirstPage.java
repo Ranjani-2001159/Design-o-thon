1145f80cc62c4f68a59ad860a7012329417a0459
Ranjani-2001159 <91516492+Ranjani-2001159@users.noreply.github.com>
2021-09-28 22:09:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/a2001124_2001159/FirstPage.java
@@ -0,0 +1,14 @@
+package com.example.a2001124_2001159;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
